1df16cb3600dc23a68b066c745285669b83b9dca
shikuan.xu <shikuan.xu@advance.ai>
2019-10-17 11:47:42 +0800

    update collection utils

--- sum_test.go
+++ sum_test.go
@@ -16,9 +16,58 @@ func TestCollection_Sum(t *testing.T) {
 }
 
 func TestCollection_User(t *testing.T) {
-	type User struct {
-		Id     int64
-		Name   string
-		Status bool
+	type Product struct {
+		Id       int64
+		Name     string
+		Status   bool
+		Quantity int
+		Price    int64
 	}
+
+	type ProductGroup struct {
+		Id       int64
+		Name     string
+		Products []Product
+	}
+	var groups []ProductGroup
+	groups = append(groups, ProductGroup{Id: 1, Name: "group-1", Products: []Product{
+		{Id: 11, Name: "p11", Status: true, Quantity: 3, Price: 8500},
+		{Id: 12, Name: "p12", Status: false, Quantity: 5, Price: 1500},
+		{Id: 13, Name: "p13", Status: true, Quantity: 8, Price: 3800},
+	}})
+	groups = append(groups, ProductGroup{Id: 2, Name: "group-2", Products: []Product{
+		{Id: 21, Name: "p21", Status: true, Quantity: 8, Price: 2800},
+		{Id: 22, Name: "p22", Status: false, Quantity: 3, Price: 4500},
+		{Id: 23, Name: "p23", Status: false, Quantity: 2, Price: 3500},
+	}})
+	groups = append(groups, ProductGroup{Id: 3, Name: "group-3", Products: []Product{
+		{Id: 31, Name: "p31", Status: true, Quantity: 1, Price: 7777},
+		{Id: 32, Name: "p32", Status: true, Quantity: 2, Price: 8888},
+		{Id: 33, Name: "p33", Status: true, Quantity: 3, Price: 9999},
+	}})
+
+	var expect int64 = 161850
+	var expectFilter int64 = 133850
+	result, err := Stream(groups).Sum(func(pg ProductGroup) int64 {
+		resultTotal, err := Stream(pg.Products).Sum(func(product Product) int64 {
+			return product.Price * int64(product.Quantity)
+		})
+		assert.Nil(t, err)
+		return resultTotal
+	})
+	assert.Nil(t, err)
+	assert.EqualValues(t, expect, result)
+
+	result, err = Stream(groups).Sum(func(pg ProductGroup) int64 {
+		resultTotal, err := Stream(pg.Products).Filter(func(product Product) bool {
+			return product.Status == true
+		}).Sum(func(product Product) int64 {
+			return product.Price * int64(product.Quantity)
+		})
+		assert.Nil(t, err)
+		return resultTotal
+	})
+	assert.Nil(t, err)
+	assert.EqualValues(t, result, expectFilter)
+
 }
